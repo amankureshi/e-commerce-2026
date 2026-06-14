839d0289e951b535194c72fa3f65463fad037df0
Aman kureshi <amankureshi78692@gmail.com>
2026-06-14 19:49:55 +0530

    added shop components

---
+++ src/Components/Shop.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Shop = () => {
+  return (
+    <div id="shop" className="max-padd-container py-2 bg-[#f8f7f4]">
+      <h3 className="text-4xl font-bold">Our Product</h3>
+      <hr />
+      <div className="h-[2px] md:w-1/2 max-w-96 bg-gradient-to-l from-transparent via-black to-black mb-24 border-none rounded">
+        shop items
+      </div>
+    </div>
+  );
+};
+
+export default Shop;
